840eec9e4bf805ea3796a3d265ff74b533f8eb03
kczulko <karol.czulkowski@gmail.com>
2016-05-26 15:06:14 +0200

    Additional exercises added

--- src/main/scala/com/kczulko/chapter2/Exercise_2_2.scala
+++ src/main/scala/com/kczulko/chapter2/Exercise_2_2.scala
@@ -19,4 +19,15 @@ object Exercise_2_2 {
 
     loop(0)
   }
+
+  def partial1[A, B, C](a: A, f: (A,B) => C): B => C = (param) => f(a, param)
+
+  // exercise 2.3
+  def curry[A,B,C](f: (A,B) => C): A => (B => C) = (a) => f(a, _)
+
+  // exercise 2.4
+  def uncurry[A,B,C](f: A => B => C): (A,B) => C = (a, b) => f(a)(b)
+
+  // excercise 2.5
+  def compose[A,B,C](f: B => C, g: A => B): A => C = (a) => f(g(a))
 }
