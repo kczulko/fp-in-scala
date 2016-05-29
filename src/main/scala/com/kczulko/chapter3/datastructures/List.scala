e7345d9507d22796aef7ddb571422fa36ec95c3d
kczulko <karol.czulkowski@gmail.com>
2016-05-29 21:13:19 +0200

    Work in progress

--- src/main/scala/com/kczulko/chapter3/datastructures/List.scala
+++ src/main/scala/com/kczulko/chapter3/datastructures/List.scala
@@ -49,14 +49,76 @@ object MyList {
     case Cons(head, tail) => head + sum(tail)
   }
 
-  def product(ds: MyList[Double]): Double = ds match {
-    case Nil => 1.0
-    case Cons(0.0, _) => 0.0
-    case Cons(head, tail) => head * product(tail)
+  def product(ds: MyList[Double]): Double = foldRight(ds, 1.0)(_ * _)
+
+  def sum2(ints: MyList[Int]): Int = foldRight(ints, 0)(_ + _)
+
+  def foldRight[A,B](list: MyList[A], base: B)(f: (A, B) => B): B = list match {
+    case Nil => base
+    case Cons(head, tail) => f(head, foldRight(tail, base)(f))
   }
+
+  def foldRight2[A,B](list: MyList[A], base: B)(f: (A, B) => B): B =
+    foldLeft(list, base)((x,y) => f(y,x))
+
+  @tailrec
+  def foldLeft[A,B](list: MyList[A], base: B)(f: (B,A) => B): B = list match {
+    case Nil => base
+    case Cons(head, tail) => foldLeft(tail, f(base, head))(f)
+  }
+
+  @tailrec
+  def foldLeft2[A,B](list: MyList[A], base: B)(f: (B,A) => B): B = {
+    println("Called with params:")
+    println("list : " + list)
+    println("base : " + base)
+    println("f : " + f)
+
+    list match {
+      case Nil => base
+      case Cons(head, tail) => foldLeft2(tail, f(base, head))(f)
+    }
+  }
+
+  def concat[A](listOfLists: MyList[MyList[A]]): MyList[A] = {
+    foldLeft(listOfLists, Nil:MyList[A])((x,y) => append(x, y))
+  }
+
+  def append2[A](a1: MyList[A], a2: MyList[A]): MyList[A] =
+    foldLeft(reverse(a1), a2)((x,y) => Cons(y, x))
+
+  def append3[A](a1: MyList[A], a2: MyList[A]): MyList[A] =
+    foldRight(a1, a2)((x,y) => Cons(x, y))
+
+  def sumAsFoldLeft(ints: MyList[Int]): Int = foldLeft(ints, 0)(_ + _)
+
+  def productAsFoldLeft(ints: MyList[Double]): Double = foldLeft(ints, 1.0)(_ * _)
+
+  def lengthAsFoldLeft[A](list: MyList[A]): Int = foldLeft(list, 0)((x,y)=>x + 1)
+
+  def length[A](list: MyList[A]): Int = foldRight(list, 0)((x,y)=>1+y)
+
+  def reverse[A](list: MyList[A]) = foldLeft(list, Nil: MyList[A])((x,y) => Cons(y,x))
 
   def apply[A](as: A*): MyList[A] = if (as isEmpty) Nil
                                     else Cons(as.head, apply(as.tail: _*))
 
+  def map[A,B](list: MyList[A])(f: A => B) : MyList[B] =
+    foldRight(list, Nil:MyList[B])((x,y) => Cons(f(x),y))
 
+  def filter[A](as: MyList[A])(f: A => Boolean): MyList[A] =
+    foldRight(as, Nil:MyList[A])((x,y) => if(!f(x)) Cons(x, y) else y)
+
+  def flatMap[A,B](as: MyList[A])(f: A => MyList[B]): MyList[B] =
+    foldRight(as, Nil:MyList[B])((x,y) => append(f(x),y))
+
+  def filter2[A](as: MyList[A])(f: A => Boolean): MyList[A] =
+    flatMap(as)(i => if (f(i)) Nil else MyList(i))
+
+  def zipWith[A,B](first:MyList[A], second: MyList[A])(f: (A,A) => B): MyList[B] = (first,second) match {
+    case (_, Nil) => Nil
+    case (Nil, _) => Nil
+    case (Cons(aHead,aTail), Cons(bHead,bTail))
+      => Cons(f(aHead, bHead), zipWith(aTail,bTail)(f))
+  }
 }
--- src/test/scala/com/kczulko/chapter3/datastructures/MyListTests.scala
+++ src/test/scala/com/kczulko/chapter3/datastructures/MyListTests.scala
@@ -1,6 +1,6 @@
 package com.kczulko.chapter3.datastructures
 
-import com.kczulko.chapter3.datastructures.MyList.{drop, dropWhile, setHead, tail}
+import com.kczulko.chapter3.datastructures.MyList._
 import org.scalatest.{FlatSpec, FunSuite, Matchers}
 
 class MyListTests extends FlatSpec with Matchers {
@@ -32,6 +32,74 @@ class MyListTests extends FlatSpec with Matchers {
   }
 
   "init" should "return a list without it last element" in {
-    MyList.init(MyList(1,2,3,4)) shouldEqual MyList(1,2,3)
+    init(MyList(1,2,3,4)) shouldEqual MyList(1,2,3)
+  }
+
+  "length" should "behave list normal lenght function" in {
+    MyList.length(MyList(1,2,3,4)) shouldEqual List(1,2,3,4).length
+  }
+
+  "foldLeft" should "behave like foldRight" in {
+    foldLeft(MyList(1,2,3,4), 0)(_+_) shouldEqual MyList.foldRight(MyList(1,2,3,4), 0)(_+_)
+  }
+
+  "lengthAsFoldLeft" should "behave like non-tail-recursive length" in {
+    lengthAsFoldLeft(MyList(1,2,3,4,8)) shouldEqual MyList.length(MyList(2,2,2,2,2))
+  }
+
+  "reverse" should "work as expected" in {
+    reverse(MyList(1,2,3)) shouldEqual MyList(3,2,1)
+  }
+
+  "append2" should "return second parameter when called with Nil" in {
+    append2(Nil, MyList(3,4)) shouldEqual MyList(3,4)
+  }
+
+  it should "return results as append" in {
+    append2(MyList(1,2), MyList(3,4)) shouldEqual MyList.append(MyList(1,2), MyList(3,4))
+  }
+
+  "append3" should "return second parameter when called with Nil" in {
+    append3(Nil, MyList(3,4)) shouldEqual MyList(3,4)
+  }
+
+  it should "return results as append" in {
+    append3(MyList(1,2), MyList(3,4)) shouldEqual MyList.append(MyList(1,2), MyList(3,4))
+  }
+
+  it should "return the same list when appended to Nil list" in {
+    append3(MyList(1,2), Nil) shouldEqual MyList(1,2)
+  }
+
+  "concat" should "return expected results" in {
+    concat(MyList(MyList(1,2), MyList(3,4))) shouldEqual MyList(1,2,3,4)
+  }
+
+  "map" should "change each element accordingly with applied function" in {
+    map(MyList(1,2))(_+3) shouldEqual MyList(4,5)
+  }
+
+  it should "transform list of doubles to list of strings" in {
+    map(MyList(1.0,2.0,3.0))(_.toString) shouldEqual MyList("1.0","2.0","3.0")
+  }
+
+  "filter" should "remove all matching items from the list" in {
+    filter(MyList(1,2,3,4,5,6))(_ % 2 == 0) shouldEqual MyList(1,3,5)
+  }
+
+  "flatMap" should "return a list of results" in {
+    flatMap(MyList(1,2,3))(i => MyList(i,i)) shouldEqual MyList(1,1,2,2,3,3)
+  }
+
+  it should "return a flatten map" in {
+    flatMap(MyList(MyList(1,2), MyList(3,4)))(i=>i) shouldEqual MyList(1,2,3,4)
+  }
+
+  "filter2" should "remove matching elements using underlying flatMap implementation" in {
+    filter2(MyList(1,2,3,4,5))(_ % 2==0) shouldEqual MyList(1,3,5)
+  }
+
+  "zipWith" should "add elements of two lists when that fuction is applied as an arg" in {
+    zipWith(MyList(1,2,3), MyList(4,5,6))(_ + _) shouldEqual MyList(5,7,9)
   }
 }
