0b77e4031367a8f20dc2ed833f560e5d9f326c52
kczulko <karol.czulkowski@gmail.com>
2016-06-01 23:39:47 +0200

    Chapter 2 finished

--- src/main/scala/com/kczulko/chapter3/datastructures/BinTree.scala
+++ src/main/scala/com/kczulko/chapter3/datastructures/BinTree.scala
@@ -7,24 +7,19 @@ case class Leaf[A](value: A) extends BinTree[A]
 case class Branch[A](left: BinTree[A], right: BinTree[A]) extends BinTree[A]
 
 object BinTree {
-  def size[A](binTree: BinTree[A]): Int = binTree match {
-    case Leaf(_) => 1
-    case Branch(l, r) => 1 + size(l) + size(r)
-  }
+  def size[A](binTree: BinTree[A]): Int = fold(binTree, (a : A) => 1)((x,y) => 1 + x() + y())
 
-  def maximum(binTree: BinTree[Int]): Int = binTree match {
-    case Leaf(v) => v
-    case Branch(l, r) => maximum(l) max maximum(r)
-  }
+  def maximum(binTree: BinTree[Int]): Int = fold(binTree, (a: Int) => a)(_() max _())
 
-  def depth[A](binTree: BinTree[A]): Int = binTree match {
-    case Leaf(_) => 1
-    case Branch(l,r) => (depth(r) + 1).max(depth(l) + 1)
-  }
+  def depth[A](binTree: BinTree[A]): Int =
+    fold(binTree, (a: A) => 1)((x,y) => (x() + 1).max(y() + 1))
 
-  def map[A,B](binTree: BinTree[A])(f: A => B): BinTree[B] = binTree match {
-    case Leaf(v) => Leaf(f(v))
-    case Branch(l,r) => Branch(map(l)(f), map(r)(f))
+  def map[A,B](binTree: BinTree[A])(f: A => B): BinTree[B] =
+    fold(binTree, (a: A) => Leaf(f(a)): BinTree[B])((x,y) => Branch(x(), y()))
+
+  def fold[A,B](binTree: BinTree[A], b: A => B)(f: (() => B, () => B) => B): B = binTree match {
+    case Leaf(v) => b(v)
+    case Branch(l, r) => f(() => fold(l,b)(f), () => fold(r,b)(f))
   }
 }
 
