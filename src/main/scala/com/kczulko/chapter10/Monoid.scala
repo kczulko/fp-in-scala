7030bc7aa70cb1a46fc67e5c31916209eaf9306f
kczulko <karol.czulkowski@gmail.com>
2016-12-06 22:35:48 +0100

    Still some tasks TODO from Monoids chapter

--- src/main/scala/com/kczulko/chapter10/Monoid.scala
+++ src/main/scala/com/kczulko/chapter10/Monoid.scala
@@ -38,4 +38,50 @@ object Monoids {
     override def op(a1: (A) => A, a2: (A) => A): (A) => A = a1 compose a2
     override def zero: (A) => A = x => x
   }
+
+  def foldMap[A,B](as: List[A], m: Monoid[B])(f: A => B): B =
+    as.foldLeft(m.zero)((b,a) => m.op(b,f(a)))
+
+  def foldRight[A,B](as: List[A], zero: B)(f: (A,B) => B): B =
+    foldMap(as, endoMonoid: Monoid[B => B])(f.curried)(zero)
+
+  def foldMapV[A,B](v: IndexedSeq[A], m: Monoid[B])(f: A => B): B = v.length match {
+    case 0 => m.zero
+    case 1 => f(v.head)
+    case _ => {
+      val splitted = v.splitAt(v.length/2)
+      m.op(foldMapV(splitted._1, m)(f), foldMapV(splitted._2, m)(f))
+    }
+  }
+
+//  def isOrdered[A : Ordering](v: IndexedSeq[A]): Boolean = {
+//  }
+
+  def wcMonoid = new Monoid[WC] {
+    override def op(a1: WC, a2: WC): WC = (a1, a2) match {
+      case (Stub(a), Stub(b)) => Stub(a + b)
+      case (Stub(a), Part(l,w,r)) => Part(a+l, w, r)
+      case (Part(l,w,r), Stub(a)) => Part(l, w, r + a)
+      case (Part(l1, w1, r1), Part(l2, w2, r2)) => Part(l1, w1 + w2 + (if ((r1 + l2).isEmpty) 0 else 1), r2)
+    }
+    override def zero: WC = Stub("")
+  }
+
+  def countWords(s: String): Int = {
+    def wc(c: Char): WC = c.isWhitespace match {
+      case true => Part("", 0, "")
+      case _ => Stub(c.toString)
+    }
+    def unstub(s: String) = s.length min 1
+    foldMapV(s.toIndexedSeq, wcMonoid)(wc) match {
+      case Stub(b) => unstub(b)
+      case Part(l,w,r) => unstub(l) + w + unstub(r)
+    }
+  }
+
+  def productMonoid[A,B](ma: Monoid[A], mb: Monoid[B]): Monoid[(A,B)] = new Monoid[(A, B)] {
+    override def op(a1: (A, B), a2: (A, B)): (A, B) = (ma.op(a1._1, a2._1), mb.op(a1._2, a2._2))
+    override def zero: (A, B) = (ma.zero, mb.zero)
+  }
+
 }
--- src/test/scala/com/kczulko/chapter10/MonoidsTests.scala
+++ src/test/scala/com/kczulko/chapter10/MonoidsTests.scala
@@ -10,4 +10,30 @@ class MonoidsTests extends FlatSpec with MonoidLaws with Matchers {
   "Boolean and" should behave like monoidOf(booleanAnd)(true,false,true)
   "Option monoid" should behave like monoidOf(optionMonoid: Monoid[Option[Boolean]])(Some(true), None, Some(true))
 //  "Endo monoid" should behave like monoidOf(endoMonoid: Monoid[Int => Int])(v=>v+2, t=>t-1, u=>u*2)
+
+  "foldMap" should "return expected result when folding whole list" in {
+    foldMap(List("1", "2", "3"), intAddition)(_.toInt) shouldEqual 6
+  }
+
+  "foldRight with foldMap impl" should "behave like 'normal' foldRight" in {
+    foldRight(List("1", "2", "3"), "")(_ + _) shouldEqual "123"
+  }
+//
+//  "isOrdered" should "work as expected" in {
+//    isOrdered(IndexedSeq(1,2,3,4)) shouldEqual true
+//    isOrdered(IndexedSeq(1,2,4,3)) shouldEqual false
+//    isOrdered(IndexedSeq(1,2)) shouldEqual true
+//    isOrdered(IndexedSeq(2,1)) shouldEqual false
+//    isOrdered(IndexedSeq(2)) shouldEqual true
+//    isOrdered(IndexedSeq()) shouldEqual true
+//  }
+
+  "wc" should "properly count words" in {
+    countWords("this is sparta!") shouldEqual 3
+  }
+
+  "productMonoid" should behave like
+    monoidOf(
+      productMonoid(optionMonoid: Monoid[Option[Boolean]], intAddition)
+    )((Some(true), 2), (Some(false), 0), (None, 3))
 }
