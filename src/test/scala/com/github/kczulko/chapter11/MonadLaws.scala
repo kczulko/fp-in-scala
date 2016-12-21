c74e8c456ffe46fcd2e12957fac24e26d78ea628
Karol Czulkowski <karol.czulkowski@intel.com>
2016-12-21 15:58:34 +0100

    Added parameter list.

--- src/test/scala/com/github/kczulko/chapter11/MonadLaws.scala
+++ src/test/scala/com/github/kczulko/chapter11/MonadLaws.scala
@@ -5,19 +5,23 @@ import org.scalatest.{FlatSpec, Matchers}
 trait MonadLaws { this: FlatSpec with Matchers =>
 
   def monadOf[A, B, C, T[U] <: { def flatMap[V](fun: U => T[V]): T[V] }]
-  (m: Monad[T], init: T[A])
+  (m: Monad[T], suts: T[A]*)
   (f: A => T[B])
   (g: B => T[C]): Unit = {
 
     it should "satisfy monad's associativity law" in {
-      m.flatMap(init)(f).flatMap(g) shouldEqual m.flatMap(init)(a => f(a).flatMap(g))
+      suts foreach { sut =>
+        m.flatMap(sut)(f).flatMap(g) shouldEqual m.flatMap(sut)(a => f(a).flatMap(g))
+      }
     }
 
     it should "satisfy identity function composition law" in {
       val f1 = m.compose[A,A,B](m.unit(_), f)
       val f2 = m.compose[A,B,B](f, m.unit(_))
 
-      m.flatMap(init)(f1) shouldEqual m.flatMap(init)(f2)
+      suts foreach { sut =>
+        m.flatMap(sut)(f1) shouldEqual m.flatMap(sut)(f2)
+      }
     }
   }
 }
