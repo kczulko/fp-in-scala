acda90ec730155a30d3fc462b6b0bd209ee7060f
Karol Czulkowski <karol.czulkowski@intel.com>
2016-12-22 15:34:41 +0100

    Applicative: finished task 2

--- src/main/scala/com/github/kczulko/chapter12/Applicative.scala
+++ src/main/scala/com/github/kczulko/chapter12/Applicative.scala
@@ -4,6 +4,7 @@ import com.github.kczulko.chapter11.Functor
 
 trait Applicative[F[_]] extends Functor[F] {
   // primitive combinators
+  def apply[A,B](fab: F[A => B])(fa: F[A]): F[B]
   def map2[A,B,C](fa: F[A], fb: F[B])(f: (A,B) => C): F[C]
   def unit[A](a: => A): F[A]
 
@@ -16,4 +17,15 @@ trait Applicative[F[_]] extends Functor[F] {
   def sequence[A](fas: List[F[A]]): F[List[A]] = traverse(fas)(identity)
   def replicateM[A](n: Int, fa: F[A]): F[List[A]] = map(fa)(List.fill(n)(_))
   def product[A,B](fa: F[A], fb: F[B]): F[(A,B)] = map2(fa,fb)((_,_))
+
+  // (apply,unit) == (map, map2)
+  def apply2[A,B](fab: F[A => B])(fa: F[A]): F[B] =
+    map2(fab, fa)(_(_))
+
+  def mapInTermsOfApply[A, B](fa: F[A])(f: A => B): F[B] =
+    apply(unit[A => B](f))(fa)
+  def map2InTermsOfApply[A,B,C](fa: F[A], fb: F[B])(f: (A,B) => C): F[C] = {
+    val fbc = mapInTermsOfApply(fa)(f.curried)
+    apply2(fbc)(fb)
+  }
 }
