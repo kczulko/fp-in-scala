81c947e4e0fa51ae3983a2f4af57064b0a049c1f
kczulko <karol.czulkowski@gmail.com>
2017-01-10 22:34:36 +0100

    Finished task 13.3

--- src/main/scala/com/github/kczulko/chapter13/free/Free.scala
+++ src/main/scala/com/github/kczulko/chapter13/free/Free.scala
@@ -2,6 +2,8 @@ package com.github.kczulko.chapter13.free
 
 import com.github.kczulko.chapter11.Monad
 
+import scala.annotation.tailrec
+
 
 sealed trait Free[F[_], A] {
   def map[B](f: A => B): Free[F,B] = flatMap(f andThen(Return(_)))
@@ -16,5 +18,34 @@ object Free {
   def freeMonad[F[_]]: Monad[({type f[a] = Free[F,a]})#f] = new Monad[({type f[a] = Free[F, a]})#f] {
     override def flatMap[A, B](ma: Free[F, A])(f: (A) => Free[F, B]): Free[F, B] = ma flatMap f
     override def unit[A](a: => A): Free[F, A] = Return(a)
+  }
+
+  @tailrec
+  def runTrampoline[A](a: Free[Function0, A]): A = a match {
+    case Return(v) => v
+    case Suspend(f) => f()
+    case FlatMap(x,f) => x match {
+      case Return(v) => runTrampoline(f(a))
+      case Suspend(r) => runTrampoline(f(r()))
+      case FlatMap(y,g) => runTrampoline(y flatMap(g(_) flatMap f))
+    }
+  }
+
+  def run[F[_],A](a: Free[F,A])(implicit monadF: Monad[F]): F[A] = {
+    @tailrec
+    def step(v: Free[F,A]): Free[F,A] = a match {
+      case FlatMap(FlatMap(x,f),g) => step(x flatMap(f(_) flatMap g))
+      case FlatMap(Return(x), f) => step(f(x))
+      case _ => v
+    }
+
+    step(a) match {
+      case Return(a) => monadF.unit(a)
+      case Suspend(a) => a
+      case FlatMap(x,f) => x match {
+        case Suspend(r) => monadF.flatMap(r)(a => run(f(a)))
+        case _ => sys.error("Impossible: `step` eliminates this cases.")
+      }
+    }
   }
 }
