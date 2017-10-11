3c1b899130b8b50ccaf1a0bc4429a71739fe0d72
Karol Czulkowski <karol.czulkowski@gmail.com>
2017-10-12 00:00:28 +0200

    Add implementation and tests for Parser chapter.

--- src/main/scala/com/github/kczulko/chapter9/Parsers.scala
+++ src/main/scala/com/github/kczulko/chapter9/Parsers.scala
@@ -1,9 +1,11 @@
 package com.github.kczulko.chapter9
 
+import scala.annotation.tailrec
 import scala.util.matching.Regex
 
 object Parsers {
   type Parser[+A] = Location => Result[A]
+
   sealed trait Result[+A] {
     def mapError(f: ParserError => ParserError) = this match {
       case Failure(pe, ic) => Failure(f(pe), ic)
@@ -11,64 +13,123 @@ object Parsers {
     }
 
     def uncommit: Result[A] = this match {
-      case Failure(pe, true) => Failure(pe, isCommited = false)
+      case Failure(pe, true) => Failure(pe, isCommitted = false)
       case _ => this
     }
   }
+
+  object Result {
+    def addCommit[A](result: Result[A], isCommitted: Boolean): Result[A] = result match {
+      case Failure(e, c) => Failure(e, c || isCommitted)
+      case _ => result
+    }
+
+    def advanceSuccess[A](result: Result[A], n: Int): Result[A] = result match {
+      case Success(a, c) => Success(a, c + n)
+      case _ => result
+    }
+  }
+
   case class Success[A](get: A, charsConsumed: Int) extends Result[A]
-  case class Failure(get: ParserError, isCommited: Boolean) extends Result[Nothing]
+
+  case class Failure(get: ParserError, isCommitted: Boolean) extends Result[Nothing]
 
   case class ParserError(stack: List[(Location, String)]) {
     def push(location: Location, msg: String): ParserError = copy(stack = (location, msg) :: stack)
-    def label[A](s: String): ParserError = ParserError(latestLocation.map((_,s)).toList)
-    def latest = stack.lastOption
-    def latestLocation = latest map (_._1)
+
+    def label[A](s: String): ParserError = ParserError(latestLocation.map((_, s)).toList)
+
+    def latest: Option[(Location, String)] = stack.lastOption
+
+    def latestLocation: Option[Location] = latest map (_._1)
   }
+
   case class Location(input: String, offset: Int = 0) {
-    lazy val line = input.substring(0,offset+1)
-    lazy val col = input.substring(0,offset+1).lastIndexOf('\n') match {
-      case -1 => offset+1
+    lazy val line = input.substring(0, offset + 1)
+    lazy val col = input.substring(0, offset + 1).lastIndexOf('\n') match {
+      case -1 => offset + 1
       case lineStart => offset - lineStart
     }
 
     def toError(msg: String): ParserError = ParserError(List((this, msg)))
+
+    def advanceBy(n: Int): Location = copy(offset = offset + n)
   }
 
   def run[A](p: Parser[A])(input: String): Either[ParserError, A] = {
-    val location = Location(input)
-    p(location) match {
-      case Success(a, _) => Right(a)
-      case Failure(pe, _) => Left(pe)
-    }
+    runL(p)(Location(input))
+  }
+
+  def runL[A](p: Parser[A])(location: Location) = p(location) match {
+    case Success(a, _) => Right(a)
+    case Failure(pe, _) => Left(pe)
   }
 
   // primitives
-  def stringP(s: String): Parser[String] = location =>
-    if (location.input.startsWith(s)) {
+  def stringP(s: String): Parser[String] = location => {
+    val startingString = location.input.substring(location.offset)
+
+    if (startingString.startsWith(s)) {
       Success(s, s.length)
+    } else if (startingString.isEmpty) {
+      Failure(location.toError(s"Expected '$s' but found '${location.input}'"), true)
     } else {
-      Failure(location.toError(s"Expected '$s' but found '${location.input}'"), isCommited = true)
+      Failure(location.toError(s"Expected '$s' but found '${location.input}'"), isCommitted = false)
+    }
+  }
+
+  def slice[A](p: Parser[A]): Parser[String] = location => {
+    @tailrec
+    def go(result: String, loc: Location): Result[String] = {
+      p(loc) match {
+        case Success(_, cc) => {
+          val w = "dupa"
+          go(result + loc.input.substring(loc.offset, loc.offset + cc), loc.advanceBy(cc))
+        }
+        case Failure(_,true) => Success(result, loc.offset)
+        case f @ Failure(_,_) => f
+      }
     }
 
-  def slice[A](p: Parser[A]): Parser[String] = ???
-  def many[A](p: Parser[A]): Parser[List[A]] = ???
-  def product[A,B](a: Parser[A], b: => Parser[B]): Parser[(A,B)] = ???
+    go("", location)
+  }
+  def many[A](p: Parser[A]): Parser[List[A]] = location => {
+    @tailrec
+    def go(res: List[A], loc: Location): Result[List[A]] = p(loc) match {
+      case Success(a, cc) => go(a :: res, loc.advanceBy(cc))
+      case Failure(_,true) => Success(res, loc.offset)
+      case f @ Failure(_,_) => f
+    }
+
+    go(Nil, location)
+  }
+
+  def product[A,B](a: Parser[A], b: => Parser[B]): Parser[(A,B)] = flatMap(a) {
+    av => map(b)(bv => (av,bv))
+  }
   def flatMap[A,B](p: Parser[A])(f: A => Parser[B]): Parser[B] = l => p(l) match {
-    case Success(a, c) => f(a)(l.copy(offset = c))
+    case Success(a, c) =>
+      val result = f(a)(l.advanceBy(c))
+      Result.advanceSuccess(
+        Result.addCommit(result, c != 0), c
+      )
     case f @ Failure(_, _) => f
   }
   def or[A](s1: Parser[A], s2: => Parser[A]): Parser[A] = l => s1(l) match {
     case Failure(_, false) => s2(l)
     case any => any
   }
-  def map[A,B](p: Parser[A])(f: A => B): Parser[B] = ???
+  def map[A,B](p: Parser[A])(f: A => B): Parser[B] = loc => p(loc) match {
+    case Success(a,cc) => Success(f(a), cc)
+    case f @ Failure(_,_) => f
+  }
+
   implicit def string(s: String): Parser[String] = stringP(s)
 
   implicit def regex(r: Regex): Parser[String] = l =>
-    r.findFirstMatchIn(l.input)
-      .map(m => Success(l.input.substring(m.start, m.end), m.end))
-      .getOrElse(Failure(l.toError(s"Cannot find $r within ${l.input}"), true))
-
+    r.findFirstMatchIn(l.input.substring(l.offset))
+      .map(m => Success(l.input.substring(l.offset).substring(m.start, m.end), l.offset + m.end - 1))
+      .getOrElse(Failure(l.toError(s"Cannot find $r within ${l.input}"), isCommitted = true))
 
   def char(c: Char): Parser[Char] = string(c.toString).map(_.charAt(0))
   def succeed[A](a: A): Parser[A] = stringP("").map(_ => a)
@@ -77,7 +138,7 @@ object Parsers {
   def many1[A](p: Parser[A]): Parser[List[A]] = map(product(p, many(p))) { case (a, list) => a :: list }
   def listOfN[A](n: Int, p: Parser[A]): Parser[List[A]] = n match {
     case i if i <= 0 => succeed(List[A]())
-    case _ => map2(p, listOfN(n -1, p))(_ :: _)
+    case _ => map2(p, listOfN(n - 1, p))(_ :: _)
   }
   def manyInTermsOfOrMap2andSucceed[A](p: Parser[A]): Parser[List[A]] =
     or(
@@ -100,10 +161,9 @@ object Parsers {
     def |[B >: A](p2: Parser[B]): Parser[B] = Parsers.or(p, p2)
     def or[B >: A](p2: => Parser[B]): Parser[B] = Parsers.or(p, p2)
     def many: Parser[List[A]] = Parsers.many(p)
+    def many1: Parser[List[A]] = Parsers.many1(p)
     def slice: Parser[String] = Parsers.slice(p)
     def map[B](f: A => B): Parser[B] = Parsers.map(p)(f)
+    def **[B](p2: Parser[B]): Parser[(A, B)] = Parsers.product(p, p2)
   }
-
-  def errorLocation(e: ParserError): Location = ???
-  def errorMessage(e: ParserError): String = ???
 }
--- src/test/scala/com/github/kczulko/chapter9/ParsersTest.scala
+++ src/test/scala/com/github/kczulko/chapter9/ParsersTest.scala
@@ -5,36 +5,82 @@ import org.scalatest.{FlatSpec, Matchers}
 import Parsers._
 import org.scalacheck.Gen
 
+import scala.language.postfixOps
+import scala.util.matching.Regex
+
 class ParsersTest extends FlatSpec with Matchers with PropertyChecks {
 
-  ignore /*"parser"*/ should "hold basic law for char parser" in {
+  "parser" should "hold basic law for char parser" in {
     Parsers.run(char('c'))('c'.toString) shouldEqual Right('c')
   }
 
-  ignore should "hold associativity law when run with alternative parsers" in {
-    Parsers.run(or(string("abra"), string("cadabra")))("abra") shouldEqual Right("abra")
-    Parsers.run(or(string("abra"), string("cadabra")))("cadabra") shouldEqual Right("cadabra")
+  "or" should "hold associativity law when run with alternative parsers" in {
+    val abraOrCadabra = "abra" or "cadabra"
+
+    Parsers.run(abraOrCadabra)("abra") shouldEqual Right("abra")
+    Parsers.run(abraOrCadabra)("cadabra") shouldEqual Right("cadabra")
   }
 
-  ignore should "repeat matches while called with listOfN" in {
-    Parsers.run(listOfN(3, "ab" | "cad"))("ababcad") shouldEqual Right("ababcad")
-    Parsers.run(listOfN(3, "ab" | "cad"))("cadabab") shouldEqual Right("cadabab")
-    Parsers.run(listOfN(3, "ab" | "cad"))("ababab") shouldEqual Right("ababab")
+  it should "handle either first or second expression" in {
+    val parser: Parser[String] ="a" | "b"
+
+    Parsers.run(parser)("b") shouldEqual Right("b")
+    Parsers.run(parser)("a") shouldEqual Right("a")
+  }
+
+  "listOfN" should "repeat matches while called with listOfN" in {
+    Parsers.run(listOfN(3, "ab" | "cad"))("ababcad") shouldEqual Right(List("ab","ab","cad"))
+    Parsers.run(listOfN(3, "ab" | "cad"))("cadabab") shouldEqual Right(List("cad","ab","ab"))
+    Parsers.run(listOfN(3, "ab" | "cad"))("ababab") shouldEqual Right(List("ab","ab","ab"))
   }
 
   "string parsers" should "return beginning of the input string" in {
     forAll(Gen.alphaLowerStr) {
       s =>
-        Parsers.run(string(""))(s) shouldEqual Right("")
-        Parsers.run(string(s))(s) shouldEqual Right(s)
+        Parsers.run("")(s) shouldEqual Right("")
+        Parsers.run(s)(s) shouldEqual Right(s)
         if (s.length > 4) {
           val substr = s.take(4)
           val reversedSubst = substr.reverse
           Parsers.run(string(substr))(s) shouldEqual Right(substr)
           if (substr != reversedSubst) {
-            Parsers.run(string(reversedSubst))(s).isLeft shouldEqual true
+            Parsers.run(reversedSubst)(s).isLeft shouldEqual true
           }
         }
     }
+  }
+
+  "many" should "produce array of proper length" in {
+    Parsers.run("a" many)("aaaaa") shouldEqual Right(List.fill(5)("a"))
+    Parsers.run("a" many)("aaaaab").isLeft shouldEqual true
+    Parsers.run("a" many)("") shouldEqual Right(List.empty)
+  }
+
+  "many1" should "fail when input string is empty" in {
+    Parsers.run("a" many1)("").isLeft shouldEqual true
+  }
+
+  "slice" should "concatenate valid result into single string as long as parser succeds" in {
+    val positive = "aaabaa"
+    val negative = "aabbcaa"
+
+    val aOrB = ("a" | "b") slice
+
+    Parsers.run(aOrB)(positive) shouldEqual Right(positive)
+    Parsers.run(aOrB)(negative).isLeft shouldEqual true
+    Parsers.run(aOrB)("") shouldEqual Right("")
+  }
+
+  "product" should "allow to join two parsers and run them sequentially" in {
+    val parser: Parser[((String, String), String)] = "a" ** "b" ** "c"
+
+    Parsers.run(parser)("abc") shouldEqual Right((("a", "b"), "c"))
+//    Parsers.run(""""""" ** "\\d+".r ** """"""")(""""2"""") shouldEqual Right((("\"", "any"), "\""))
+  }
+
+  "regex" should "allow to parse with given regex expression" in {
+    val parser: Parser[String] = """\w+""".r
+
+    (""""""" ** parser ** """"""").apply(Location(""""dz"""")) shouldEqual Success((("\"", "dz"), "\""), 4)
   }
 }
